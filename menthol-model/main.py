340528b0cf026425ba032ebed9e901c8616f690f
tapphughesn <nicholas.tapphughes@gmail.com>
2021-12-20 20:22:41 -0500

    reading smoking prevalences and RRs plus added arguments in simulation __init__

--- menthol-model/main.py
+++ menthol-model/main.py
@@ -8,6 +8,7 @@ def main(args):
     # 0 = male
     # 1 = female
 
+    # Get life tables
     life_table_dict = {}
     life_table_dict[2016] = {}
     life_table_dict[2017] = {}
@@ -19,49 +20,25 @@ def main(args):
     life_table_dict[2017][1] = pd.read_excel("../../life_tables/2017/Female/life_table_2017_female.xlsx").to_numpy()[2:-1,1]
     life_table_dict[2018][1] = pd.read_excel("../../life_tables/2018/Females/life_table_2018_female.xlsx").to_numpy()[2:-1,1]
 
-    # Use RR and smoking prevalence to calculate death rates for smokers and nonsmokers
-    use_adjusted_death_rates = True
-    wave3sr = pd.read_excel(os.path.join("..", "..", "smoking_prevalence", "Wave345_age55up_smokers_SM Dec21.xlsx")).to_numpy()[:,3]
+    # Get smoking prevalences by wave for ages 55-90
+    smoking_prevalence_dict = {}
+    smoking_prevalence_dict[2016] = pd.read_excel(os.path.join("..", "..", "smoking_prevalence", "Wave345_age55up_smokers_SM Dec21.xlsx")).to_numpy()[:,3]
+    smoking_prevalence_dict[2017] = pd.read_excel(os.path.join("..", "..", "smoking_prevalence", "Wave345_age55up_smokers_SM Dec21.xlsx")).to_numpy()[:,6]
+    smoking_prevalence_dict[2018] = pd.read_excel(os.path.join("..", "..", "smoking_prevalence", "Wave345_age55up_smokers_SM Dec21.xlsx")).to_numpy()[:,9]
 
-    if use_adjusted_death_rates:
-        smoking_prevalence_dict = {}
-        smoking_prevalence_dict[2016] = pd.read_excel(os.path.join("..", "..", "smoking_prevalence", "Wave345_age55up_smokers_SM Dec21.xlsx")).to_numpy()[:,3]
-        smoking_prevalence_dict[2017] = pd.read_excel(os.path.join("..", "..", "smoking_prevalence", "Wave345_age55up_smokers_SM Dec21.xlsx")).to_numpy()[:,6]
-        smoking_prevalence_dict[2018] = pd.read_excel(os.path.join("..", "..", "smoking_prevalence", "Wave345_age55up_smokers_SM Dec21.xlsx")).to_numpy()[:,9]
+    # Get Releative Risks for current smokers vs nonsmoker and former smoker vs current smoker
+    csvnsRR = pd.read_excel(os.path.join("..", "..", "smoking_prevalence", "current_smoker_mortality_vs_nonsmoker.xlsx")).to_numpy()[:,1:]
+    fsvcsRR = pd.read_excel(os.path.join("..", "..", "smoking_prevalence", "former_smoker_mortality_vs_current_smoker.xlsx")).to_numpy()[:,1:]
 
-        # life table dict will use indices year, is_female, is_smoker
-        new_life_table_dict = {}
-        new_life_table_dict[2016] = {}
-        new_life_table_dict[2017] = {}
-        new_life_table_dict[2018] = {}
-        new_life_table_dict[2016][0] = {}
-        new_life_table_dict[2017][0] = {}
-        new_life_table_dict[2018][0] = {}
-        new_life_table_dict[2016][1] = {}
-        new_life_table_dict[2017][1] = {}
-        new_life_table_dict[2018][1] = {}
+    print(csvnsRR)
+    print(fsvcsRR)
+    quit()
 
-        new_life_table_dict[2016][0][0] = pd.read_excel("../../life_tables/2016/Males/life_table_2016_male.xlsx").to_numpy()[2:-1,1]
-        new_life_table_dict[2017][0][0] = pd.read_excel("../../life_tables/2017/Male/life_table_2017_male.xlsx").to_numpy()[2:-1,1]
-        new_life_table_dict[2018][0][0] = pd.read_excel("../../life_tables/2018/Males/life_table_2018_male.xlsx").to_numpy()[2:-1,1]
-        new_life_table_dict[2016][1][0] = pd.read_excel("../../life_tables/2016/Females/life_table_2016_female.xlsx").to_numpy()[2:-1,1]
-        new_life_table_dict[2017][1][0] = pd.read_excel("../../life_tables/2017/Female/life_table_2017_female.xlsx").to_numpy()[2:-1,1]
-        new_life_table_dict[2018][1][0] = pd.read_excel("../../life_tables/2018/Females/life_table_2018_female.xlsx").to_numpy()[2:-1,1]
-
-        new_life_table_dict[2016][0][1] = pd.read_excel("../../life_tables/2016/Males/life_table_2016_male.xlsx").to_numpy()[2:-1,1]
-        new_life_table_dict[2017][0][1] = pd.read_excel("../../life_tables/2017/Male/life_table_2017_male.xlsx").to_numpy()[2:-1,1]
-        new_life_table_dict[2018][0][1] = pd.read_excel("../../life_tables/2018/Males/life_table_2018_male.xlsx").to_numpy()[2:-1,1]
-        new_life_table_dict[2016][1][1] = pd.read_excel("../../life_tables/2016/Females/life_table_2016_female.xlsx").to_numpy()[2:-1,1]
-        new_life_table_dict[2017][1][1] = pd.read_excel("../../life_tables/2017/Female/life_table_2017_female.xlsx").to_numpy()[2:-1,1]
-        new_life_table_dict[2018][1][1] = pd.read_excel("../../life_tables/2018/Females/life_table_2018_female.xlsx").to_numpy()[2:-1,1]
-
-        
-
-
-
+    # Get population data
     pop_file_name = "../../path_data/age_individual_October21_renamed.xlsx"
     pop_df = pd.read_excel(pop_file_name)
 
+    # Get logistic regression betas
     beta234_f = "../../beta_estimates/beta_estimates_234.xlsx"
     beta15_f = "../../beta_estimates/beta_estimates_15.xlsx"
     beta234_arr = pd.read_excel(beta234_f).to_numpy()[:,2:]
